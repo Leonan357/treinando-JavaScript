2516b484a1aa675bb4dc5481dc85d50078183635
Leonan357 <leonan.leo357@gmail.com>
2019-09-06 21:52:58 -0300

    Versao 1

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    
+    $(".red").click(function() {
+        $(this).fadeOut();             
+    });
+    
+    $(".green").click(function () {
+        $(".blue").hide();
+    });
+    
+    $(".yellow").click(function() {
+        $(".blue").fadeIn();
+        $(".green").hide();
+    });
+    
+    $(".blue").click(function () {
+        $(".red, .green, .blue, .yellow").fadeToggle();
+    });
+});
